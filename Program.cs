164a9bae41e88eebb018510e0892ad5cfc5e6eb6
filipe <filipe@Debian-11.lan>
2023-06-25 21:04:44 +0100

    [AKF] - Update .sln and .csproj

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-// using BlazorBootstrap;
-using CurriculumVitae;
+using AKarateFelgueiras;
 using MudBlazor.Services;
 
 internal class Program
@@ -13,7 +12,6 @@ internal class Program
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-        // builder.Services.AddBlazorBootstrap(); // Add this line
         builder.Services.AddMudServices();
 
         await builder.Build().RunAsync();
